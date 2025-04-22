76c7ec6323ef679dfef511088e81133d3b745da4
agrand24 <adam.grand@gmail.com>
2025-04-22 14:29:37 +0200

    init

--- MTSM_qgis/scripts/MTSM_python_modules.py
+++ MTSM_qgis/scripts/MTSM_python_modules.py
@@ -1,31 +1,31 @@
 import traceback
 
-# try:
-import pandas as pd
-import geopandas as gpd
-import numpy as np
-import os
-import shutil
-import warnings
-from lxml import etree
-from tabulate import tabulate
-import re
-from datetime import datetime,timedelta,time,date
-import time
-import errno, stat
-import ppigrf
-from shapely import Point,Polygon,LineString,MultiPolygon,MultiLineString
-from pathlib import Path
-import matplotlib.pyplot as plt
-from matplotlib.ticker import MultipleLocator
-from astral import LocationInfo
-from astral.sun import sun
-import pyperclip
+try:
+	import pandas as pd
+	import geopandas as gpd
+	import numpy as np
+	import os
+	import shutil
+	import warnings
+	from lxml import etree
+	from tabulate import tabulate
+	import re
+	from datetime import datetime,timedelta,time,date
+	import time
+	import errno, stat
+	import ppigrf
+	from shapely import Point,Polygon,LineString,MultiPolygon,MultiLineString
+	from pathlib import Path
+	import matplotlib.pyplot as plt
+	from matplotlib.ticker import MultipleLocator
+	from astral import LocationInfo
+	from astral.sun import sun
+	import pyperclip
 
-warnings.simplefilter(action='ignore', category=FutureWarning)
-warnings.filterwarnings('ignore', 'GeoSeries.notna', UserWarning)
-pd.set_option('mode.chained_assignment', None)
-# except Exception as error:
-# 	traceback.print_exc()
-# 	input('Press ENTER to continue!')
+	warnings.simplefilter(action='ignore', category=FutureWarning)
+	warnings.filterwarnings('ignore', 'GeoSeries.notna', UserWarning)
+	pd.set_option('mode.chained_assignment', None)
+except Exception as error:
+	traceback.print_exc()
+	input('Press ENTER to continue!')
 
